221249c6906ea1e93821e42f57df087c786eb25a
Christopher Cabreros <chriscabreros27@gmail.com>
2016-05-18 00:52:33 -0700

    Christina merge works correctly.

--- app/src/main/java/com/segfault/closetmanager/OutfitGenActivity.java
+++ app/src/main/java/com/segfault/closetmanager/OutfitGenActivity.java
@@ -11,7 +11,6 @@ import android.view.View;
  */
 public class OutfitGenActivity extends BaseActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
