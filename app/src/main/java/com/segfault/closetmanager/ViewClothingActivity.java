c4f4e9411bcfc2de4e183b3b0629a2c1ed39246b
Christopher Cabreros <chriscabreros27@gmail.com>
2016-05-18 01:01:09 -0700

    Christina merge works correctly again

--- app/src/main/java/com/segfault/closetmanager/ViewClothingActivity.java
+++ app/src/main/java/com/segfault/closetmanager/ViewClothingActivity.java
@@ -1,6 +1,7 @@
 package com.segfault.closetmanager;
 
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -11,6 +12,16 @@ public class ViewClothingActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_clothing);
+
+        // set pref_layout toolbar
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(true);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
 
         //Recreate bottom bar here because the account has not been created
         View bottomBarView = findViewById(R.id.view_clothing_bottom_bar);
